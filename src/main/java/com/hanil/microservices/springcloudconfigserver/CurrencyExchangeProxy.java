f0ce203529f2d17781381fbf7f3dae3c61c1365b
Hanil Trivedi <trivedihanil@gmail.com>
2021-09-11 16:56:56 +0530

    Step 22 - Load Balancing with Eureka, Feign & Spring Cloud LoadBalancer
    #### /currency-conversion-service/src/main/java/com/hanil/microservices/currencyconversionservice/CurrencyExchangeProxy.java Modified
    
    +@FeignClient(name = "currency-exchange") // remove the base url (ip and port)
    +//and the feign will automatically identify the port/ip as per eureka load balancing using Spring Cloud LoadBalancer)
    
    //@FeignClient(name="currency-exchange", url="localhost:8000")
    @FeignClient(name="currency-exchange")
    public interface CurrencyExchangeProxy {

--- src/main/java/com/hanil/microservices/springcloudconfigserver/CurrencyExchangeProxy.java
+++ src/main/java/com/hanil/microservices/springcloudconfigserver/CurrencyExchangeProxy.java
@@ -4,13 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-@FeignClient(name="currency-exchange", url="localhost:8000")
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name = "currency-exchange") // remove the base url (ip and port)
+//and the feign will automatically identify the port/ip as per eureka load balancing using Spring Cloud LoadBalancer)
 public interface CurrencyExchangeProxy {
-	
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public CurrencyConversion retrieveExchangeValue(
-			@PathVariable("from") String from,
-			@PathVariable("to") String to);
+	public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
